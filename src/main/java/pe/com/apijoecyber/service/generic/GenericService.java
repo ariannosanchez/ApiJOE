ef5f4378101146734545d36c0da566c24884a097
arisama <109788200+arisama@users.noreply.github.com>
2023-02-06 17:54:03 -0500

    GenericService
    Añadiendo el generic service

---
+++ src/main/java/pe/com/apijoecyber/service/generic/GenericService.java
@@ -0,0 +1,14 @@
+package pe.com.apijoecyber.service.generic;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface GenericService<T> {
+
+    List<T> findAll();
+    List<T> findAllCustom();
+    T add(T t);
+    Optional<T> findById(Long id);
+    T update(T t);
+    T delete(T t);
+}
