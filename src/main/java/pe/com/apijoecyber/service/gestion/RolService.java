95fa1b9bc122822e7e67fb811aa25181bd52c52d
arisama <109788200+arisama@users.noreply.github.com>
2023-02-06 17:55:23 -0500

    RolService
    Añadiendo RolService

---
+++ src/main/java/pe/com/apijoecyber/service/gestion/RolService.java
@@ -0,0 +1,7 @@
+package pe.com.apijoecyber.service.gestion;
+
+import pe.com.apijoecyber.entity.base.gestion.RolEntity;
+import pe.com.apijoecyber.service.generic.GenericService;
+
+public interface RolService extends GenericService<RolEntity> {
+}
