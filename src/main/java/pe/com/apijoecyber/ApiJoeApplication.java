e4218f4d0f025e415bd9629168f1ed593a3297b2
arisama <109788200+arisama@users.noreply.github.com>
2023-02-06 14:08:58 -0500

    Proyecto Spring

---
+++ src/main/java/pe/com/apijoecyber/ApiJoeApplication.java
@@ -0,0 +1,13 @@
+package pe.com.apijoecyber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/apijoecyber/ApiJoeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.apijoecyber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
