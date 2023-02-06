52a253996d7b976dc6508f87c7d53aabe7f304b6
arisama <109788200+arisama@users.noreply.github.com>
2023-02-06 15:06:27 -0500

    Prueba

--- src/main/java/pe/com/apijoecyber/Prueba.java
+++ src/main/java/pe/com/apijoecyber/Prueba.java
@@ -2,10 +2,8 @@ package pe.com.apijoecyber;
 
 public class Prueba {
     String hola = "Hola a todos esto es una prueba";
-<<<<<<< HEAD
     String hola1 = "Hola asjdsahdj";
-=======
     String ca = "z<z";
     String z = "123";
->>>>>>> d5ca77f303391b6681ed86f72bc35cb43937b9aa
+    String cambio = "asdasd";
 }
