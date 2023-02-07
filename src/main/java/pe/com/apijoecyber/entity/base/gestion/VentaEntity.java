3c9c73bd661066ef960687bac0b42dfdfe204f74
PercyAlex3211 <abelloorizano@gmail.com>
2023-02-06 21:26:28 -0500

    Venta Terminado pd: te quiero <3
    xdddd

---
+++ src/main/java/pe/com/apijoecyber/entity/base/gestion/VentaEntity.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.com.apijoecyber.entity.base.gestion;
+
+/**
+ *
+ * @author Percy
+ */
+public class VentaEntity {
+    
+}
